5154b25704e182a8515eb68bbd9d7cb637fa5030
Jason Leitch <jasonleitch0@gmail.com>
2022-12-06 14:49:53 +0000

    chore: add CIS compliance

--- outputs.tf
+++ outputs.tf
@@ -8,7 +8,9 @@ locals {
         requires_identity = {
             "NIST SP 800-53 rev. 5" = "/providers/Microsoft.Authorization/policySetDefinitions/179d1daa-458f-4e47-8086-2a68d0d6c38f"
         }
-        no_required_identity = {}
+        no_required_identity = {
+          "CIS Benchmark v1.4.0" = "/providers/Microsoft.Authorization/policySetDefinitions/c3f5c4d9-9a1d-4a99-85c0-7f93e384d5c5"
+        }
       }
       custom_policy = {}
     }
