bd88473bf390e8720e02505819a0e8f45b8a768d
Jason Leitch <jasonleitch0@gmail.com>
2022-12-14 09:26:21 +0000

    chore: slurp in policy files

--- outputs.tf
+++ outputs.tf
@@ -4,6 +4,9 @@ locals {
   archetype                       = var.archetype
   location                        = "uksouth"
 
+  templates_path = "${path.module}/policies/parameters"
+  list_of_policy_parameter_files = tolist(fileset(local.templates_path, "**.json"))
+
   // file path to search based on archetype
   initiatives_path                = "${path.module}/policies/${local.archetype}/initiatives"
   definitions_path                = "${path.module}/policies/${local.archetype}/definitions"
@@ -11,10 +14,12 @@ locals {
   list_of_policy_definition_files = tolist(fileset(local.definitions_path, "**.json"))
 
   // ignored if no templating is required; uses ${} syntax within .json tmpl
+  // build out for when var interoplation is required in params or policy
   common_template_values = {
     scope    = local.scope
     location = local.location
   }
+
 
   // json to HCL or null
   templated_map_of_policy_initiative_files = try(length(local.list_of_policy_initiative_files) > 0, false) ? {
@@ -27,6 +32,33 @@ locals {
     for filepath in local.list_of_policy_definition_files :
     filepath => jsondecode(templatefile("${local.definitions_path}/${filepath}", local.common_template_values))
   } : null
+
+  # templated_map_of_policy_parameter_files = try(length(local.list_of_policy_parameter_files) > 0, false) ? {
+  #   for filepath in local.list_of_policy_parameter_files :
+  #   filepath => jsondecode(templatefile("${local.templates_path}/${filepath}", local.common_template_values))
+  # } : null
+
+  # templated_map_of_policy_parameter_files = try(length(local.list_of_policy_parameter_files) > 0, false) ? [
+  #   for filepath in local.list_of_policy_parameter_files : jsondecode(templatefile("${local.templates_path}/${filepath}", local.common_template_values))
+  #  ] : null
+}
+
+// built ins do not require creation only deployment
+locals {
+  archetypes = {
+    root = {
+      builtin = [
+          {
+            name =  "NIST-SP-800-53-rev-5"
+            id = "/providers/Microsoft.Authorization/policySetDefinitions/179d1daa-458f-4e47-8086-2a68d0d6c38f"
+          },
+          {
+            name = "CIS-Benchmark-v1.4.0"
+            id = "/providers/Microsoft.Authorization/policySetDefinitions/c3f5c4d9-9a1d-4a99-85c0-7f93e384d5c5"
+          }
+      ]
+    }
+  }
 }
 
 output "initiatives" {
@@ -37,22 +69,10 @@ output "definitions" {
   value = local.templated_map_of_policy_definition_files
 }
 
-// builtin policy by policy_name => definition_id
-locals {
-  archetypes = {
-    root = {
-      builtin = {
-        requires_identity = {
-            "NIST SP 800-53 rev. 5" = "/providers/Microsoft.Authorization/policySetDefinitions/179d1daa-458f-4e47-8086-2a68d0d6c38f"
-        }
-        no_required_identity = {
-          "CIS Benchmark v1.4.0" = "/providers/Microsoft.Authorization/policySetDefinitions/c3f5c4d9-9a1d-4a99-85c0-7f93e384d5c5"
-        }
-      }
-    }
-  }
+output "builtin_definitions" {
+  value = local.archetypes[var.archetype].builtin
 }
 
-output "builtin_policy" {
-  value = local.archetypes[var.archetype].builtin
+output "parameters" {
+  value = local.template_parameters_for_policy_assignement
 }
---
+++ parameters.tf
@@ -0,0 +1,59 @@
+locals {
+  // could templates be pulled off of disk and encoded for ease and less lines??
+  // template paramaters for policy by name, to apply at assignment time
+  template_parameters_for_policy_assignement = {
+    Deploy-MDFC-Config = {
+      params = {
+        ascExportResourceGroupLocation = {
+          value = "uksouth"
+        }
+        ascExportResourceGroupName = {
+          value = "testing-law-MDFC-assignment"
+        }
+        emailSecurityContact = {
+          value = "consoto@microsoft.com"
+        }
+        enableAscForAppServices = {
+          value = "DeployIfNotExists"
+        }
+        enableAscForArm = {
+          value = "DeployIfNotExists"
+        }
+        enableAscForContainers = {
+          value = "DeployIfNotExists"
+        }
+        enableAscForDns = {
+          value = "DeployIfNotExists"
+        }
+        enableAscForKeyVault = {
+          value = "DeployIfNotExists"
+        }
+        enableAscForOssDb = {
+          value = "DeployIfNotExists"
+        }
+        enableAscForServers = {
+          value = "DeployIfNotExists"
+        }
+        enableAscForSql = {
+          value = "DeployIfNotExists"
+        }
+        enableAscForSqlOnVm = {
+          value = "DeployIfNotExists"
+        }
+        enableAscForStorage = {
+          value = "DeployIfNotExists"
+        }
+        logAnalytics = {
+          value = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/resource-group-name/providers/Microsoft.OperationalInsights/worksapces/law-name"
+        }
+      }
+    }
+    Deploy-ASC-SecContacts = {
+      params = {
+        emailSecurityContact = {
+          value = "contoso@microsoft.com"
+        }
+      }
+    }
+  }
+}
